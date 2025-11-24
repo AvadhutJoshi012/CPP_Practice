a91e3fb306cee23f7a74e611a3bf18bdc58810a9
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-11-24 10:45:06 +0530

    Added Problem statement

---
+++ Problems/VotingSystem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void calculateAge(int dob){
+    
+}
+
+int main(){
+    int dob;
+    cout<<"Enter Your Date of Birth: ";
+    cin>>dob;
+    calculateAge(dob);
+}
